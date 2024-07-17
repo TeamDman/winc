c9bec6823f1e3a3f709ffc82934b3240e8f0bad5
TeamDman <TeamDman9201@gmail.com>
2024-07-17 16:36:15 -0400

    cargo clippy --fix

--- src/monitor.rs
+++ src/monitor.rs
@@ -1,48 +1,12 @@
-use image::RgbaImage;
 #[cfg(target_arch = "x86")]
 use std::arch::x86::_mm_shuffle_epi8;
-use std::arch::x86_64::__m128i;
-use std::arch::x86_64::_mm_loadu_si128;
-use std::arch::x86_64::_mm_setr_epi8;
-#[cfg(target_arch = "x86_64")]
-use std::arch::x86_64::_mm_shuffle_epi8;
-use std::arch::x86_64::_mm_storeu_si128;
-use std::mem;
-use std::ops::Deref;
 use std::ptr;
-use std::rc::Rc;
-use std::sync::Arc;
 use widestring::U16CString;
 use windows::core::Result;
 use windows::core::PCWSTR;
-use windows::Win32::Foundation::BOOL;
 use windows::Win32::Foundation::E_INVALIDARG;
-use windows::Win32::Foundation::LPARAM;
-pub use windows::Win32::Foundation::RECT;
-use windows::Win32::Graphics::Gdi::BitBlt;
-use windows::Win32::Graphics::Gdi::CreateCompatibleBitmap;
-use windows::Win32::Graphics::Gdi::CreateCompatibleDC;
 use windows::Win32::Graphics::Gdi::CreateDCW;
-use windows::Win32::Graphics::Gdi::DeleteDC;
-use windows::Win32::Graphics::Gdi::DeleteObject;
-use windows::Win32::Graphics::Gdi::EnumDisplayMonitors;
-use windows::Win32::Graphics::Gdi::GetDIBits;
-use windows::Win32::Graphics::Gdi::GetMonitorInfoW;
-use windows::Win32::Graphics::Gdi::GetObjectW;
-use windows::Win32::Graphics::Gdi::SelectObject;
-use windows::Win32::Graphics::Gdi::SetStretchBltMode;
-use windows::Win32::Graphics::Gdi::StretchBlt;
-use windows::Win32::Graphics::Gdi::BITMAP;
-use windows::Win32::Graphics::Gdi::BITMAPINFO;
-use windows::Win32::Graphics::Gdi::BITMAPINFOHEADER;
-use windows::Win32::Graphics::Gdi::DIB_RGB_COLORS;
-use windows::Win32::Graphics::Gdi::HBITMAP;
 use windows::Win32::Graphics::Gdi::HDC;
-use windows::Win32::Graphics::Gdi::HMONITOR;
-use windows::Win32::Graphics::Gdi::MONITORINFOEXW;
-use windows::Win32::Graphics::Gdi::RGBQUAD;
-use windows::Win32::Graphics::Gdi::SRCCOPY;
-use windows::Win32::Graphics::Gdi::STRETCH_HALFTONE;
 
 use crate::prelude::get_monitor_infos;
 use crate::prelude::MonitorInfo;
